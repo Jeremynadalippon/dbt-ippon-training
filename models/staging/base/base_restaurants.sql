a3de274841ea741e68702eab79e0b62b740e8d0e
Jeremy Nadal <j.nadal@betclicgroup.com>
2022-07-27 10:20:40 +0200

    updated base model

--- models/staging/base/base_restaurants.sql
+++ models/staging/base/base_restaurants.sql
@@ -1,4 +1,4 @@
-select 
+select distinct
     identifier
     , name
     , address
--- models/staging/stg_restaurants__turnover_ordered.sql
+++ models/staging/stg_restaurants__turnover_ordered.sql
@@ -7,7 +7,7 @@ from
     {{ref('base_restaurants')}} as r
 left join
     {{ref('base_orders')}} as o
-        on o.restaurant_identifier = r.identifier
+    on o.restaurant_identifier = r.identifier
 group by 
     r.identifier
     , r.name
