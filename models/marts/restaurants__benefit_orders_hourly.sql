38ca018eb1c8fa8cd2335c682f74d7c56a6812bf
pfarey@ippon.fr <pfarey@ippon.fr>
2022-09-19 21:28:37 +0000

    scenario_6 finished

--- models/marts/restaurants__benefit_orders_hourly.sql
+++ models/marts/restaurants__benefit_orders_hourly.sql
@@ -1,13 +1,13 @@
 select 
     d.identifier
+    , date_trunc('hour',to_timestamp(odf.created_at))      as hour
     , d.name
     , count(*)                                             as nb_ordered
     , sum(d.selling_price)                                 as global_turnover
     , sum(d.production_cost)                               as global_profit
-    , date_trunc('hour',to_timestamp(odf.created_at))      as hour
 from 
     {{ref('stg_orders__dishes_flattened')}}   as odf
 left join 
     {{ref('base_dishes')}} as d
         on odf.dishes_id = d.identifier
-group by 1,2,6
+group by 1,2,3
