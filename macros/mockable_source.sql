60d9fc682a2c6d27739ca21d530ab2183c7476df
pfarey@ippon.fr <pfarey@ippon.fr>
2022-09-19 18:16:01 +0000

    change mockable_source

--- macros/mockable_source.sql
+++ macros/mockable_source.sql
@@ -1,7 +1,8 @@
-{% macro mockable_source(source_name, table_name, mocked_table_file_name) %}
-  {% if target.name == 'ci' or target.name == 'default' or target.name == 'dev' %}
-    (select * from {{ref(mocked_table_file_name)}})
-  {% else %}
-    {{ source(source_name, table_name) }}
-  {% endif %}
+{% macro mockable_source(source_name, table_name, sample_table_name) %}
+    {%- set target_name = target.name.lower() -%}
+    {% if target_name in ['ci','default', 'dev'] %}
+        {{ ref(sample_table_name) }}
+    {% else %}
+        {{ source(source_name, table_name) }}
+    {% endif %}
 {% endmacro %}
