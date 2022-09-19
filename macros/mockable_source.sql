cf6b2f270b694ccf596f3e641b4aa356b4f7878f
pfarey@ippon.fr <pfarey@ippon.fr>
2022-09-19 21:23:04 +0000

    re fix

--- macros/mockable_source.sql
+++ macros/mockable_source.sql
@@ -10,7 +10,7 @@
 
 {% macro mockable_stream(source_name, stream_name, sample_sync_name) %}
     {%- set target_name = target.name.lower() -%}
-    {% if target_name in ['ci'] %}
+    {% if target_name in ['ci','default', 'dev'] %}
         {{ ref(sample_sync_name) }}
     {% else %}
         {{ source(source_name, stream_name) }}
