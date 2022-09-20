d19263c01ebabea66b16d4d945c4da9e93226bbb
Jeremy Nadal <j.nadal@betclicgroup.com>
2022-09-20 11:30:58 +0200

    scenario_5 finished

--- models/staging/stg_restaurants__turnover_ordered.sql
+++ models/staging/stg_restaurants__turnover_ordered.sql
@@ -1,6 +1,5 @@
 select
-    r.identifier                       as restaurant_identifier
-    , r.name
+    r.name
     , r.address
     , coalesce(sum(o.amount), 0)   as turnover
 from
@@ -9,8 +8,7 @@ left join
     {{ ref('base_orders') }} as o
     on o.restaurant_identifier = r.identifier
 group by
-    r.identifier
-    , r.name
+    r.name
     , r.address
 order by
     turnover desc
