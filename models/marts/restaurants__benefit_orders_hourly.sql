b8cbe0169bbfe67d9c36cb4dad48889bfbe9a4f5
p.farey <p.farey@betclicgroup.com>
2022-09-20 11:25:23 +0200

    Scenario 8 cleaned

--- models/marts/restaurants__benefit_orders_hourly.sql
+++ models/marts/restaurants__benefit_orders_hourly.sql
@@ -6,16 +6,16 @@
   )
 }}
 
-select 
+select
     d.identifier                                           as identifier
     , d.name                                               as name
-    , date_trunc('hour',to_timestamp(odf.created_at))      as hour
+    , date_trunc('hour', to_timestamp(odf.created_at))      as hour
     , count(*)                                             as nb_ordered
     , sum(d.selling_price)                                 as global_turnover
     , sum(d.production_cost)                               as global_profit
-from 
-    {{ref('stg_orders__dishes_flattened')}}   as odf
-left join 
-    {{ref('base_dishes')}} as d
-        on odf.dish_id = d.identifier
-group by 1,2,3
+from
+    {{ ref('stg_orders__dishes_flattened') }}   as odf
+left join
+    {{ ref('base_dishes') }} as d
+    on odf.dish_id = d.identifier
+group by 1, 2, 3
--- models/staging/stg_orders__dishes_flattened.sql
+++ models/staging/stg_orders__dishes_flattened.sql
@@ -1,5 +1,5 @@
 select
     o.created_at
     , d.value as dish_id
-from {{ref('base_orders')}} as o
-    ,table(flatten(input => parse_json(o.dishes_ids),outer => true)) as d
+from {{ ref('base_orders') }} as o
+, table(flatten(input => parse_json(o.dishes_ids), outer => true)) as d
