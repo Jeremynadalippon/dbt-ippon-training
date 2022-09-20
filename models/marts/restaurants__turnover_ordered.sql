d3d580f32e0f12a9400fe927d6678d755c639d9a
Jeremy Nadal <j.nadal@betclicgroup.com>
2022-09-20 11:05:03 +0200

    scenario 7 finished

--- models/marts/restaurants__turnover_ordered.sql
+++ models/marts/restaurants__turnover_ordered.sql
@@ -1,8 +1,9 @@
 select
-    restaurant_identifier::number   as restaurant_identifier
-    , name::string                  as name
-    , address::string               as address
-    , turnover::number              as turnover
-from 
-    {{ref('stg_restaurants__turnover_ordered')}}
-limit {{ var('nb_restaurants_to_show') }}
+    row_number() over (order by turnover desc)  as rank
+    , name::string                              as name
+    , address::string                           as address
+    , turnover::number                          as turnover
+from
+    {{ ref('stg_restaurants__turnover_ordered') }}
+limit
+    {{ var('nb_restaurants_to_show') }}
