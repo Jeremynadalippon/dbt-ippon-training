b037e3acdcbde0ca0e43050e0860b84a7ac93c83
rkaiss@ippon.fr <rkaiss@ippon.fr>
2023-06-06 09:32:57 +0000

    ONE

--- macros/fromm.sql
+++ macros/fromm.sql
@@ -1,6 +1,6 @@
 {% macro mockable_source(name, table) %}
     {%- if target.name == 'dev' %}
-        {{ref("restaurants")}}
+        {{ref("sample.csv")}}
     {%- elif target.name == 'prod' %}
         {{source(name, table)}}
     {%- endif %}
